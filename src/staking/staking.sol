1c4cda05e99ec417efda4f4e0182aac9de6c15fb
Riki3248 <rw740643@gmail.com>
2024-04-08 17:12:58 +0300

    contract staking

---
+++ src/staking/staking.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+// https://solidity-by-example.org/defi/staking-rewards/
+pragma solidity ^0.8.20;
+import "@hack/like/IERC20.sol";
+import "../../../new-project/src/MyToken.sol";
+contract StakingRewards {
+    IERC20 public immutable stakingToken;
+    MyToken public immutable rewardToken;
+    address public owner;
+    uint public startAt;
+    uint public updatedAt;
+    uint public rewardPerTokenStored;
+    mapping(address => uint) public deposits;
+    uint256 percentOfDeposit;//percent from totalSupply
+    uint public totalSupply;
+    mapping(address => uint) public startDate;
+    mapping(address => uint256) public rewards;
+
+    
+//     constructor(address _stakingToken, address _rewardToken) {
+//         owner = msg.sender;
+//         stakingToken = IERC20(_stakingToken);
+//         rewardToken=new MyToken(); 
+//         totalSupply = 1000000;
+
+//     }
+    
+
+//     modifier onlyOwner() {
+//         require(msg.sender == owner, "not authorized");
+//         _;
+//     }
+//     receive() external payable {}
+
+//     function getReward(uint256 _amount)  external () {
+//         rewardToken.transferFrom(address(this), msg.sender, _amount);
+//         rewards[msg.sender]+=_amount;
+    
+//     }
+//     function Deposit(uint256 _amount)  external onlyOwner
+//      {
+//          require(_amount > 0, "amount = 0");
+//          stakingToken.transferFrom(msg.sender, address(this), _amount);
+//          totalSupply += _amount; 
+//          percentOfDeposit=_amount*100/totalSupply;
+//          deposits[msg.sender] += percentOfDeposit;
+//          startDate[msg.sender]=block.timestamp;
+//          getReward(_amount);
+//     }
+
+//     modifier isSevenDay {//Checks if 7 days have passed since the deposit
+//       uint256 today =block.timestamp;
+//       require(today-(startDate[msg.sender]) >= 7, "reward duration not finished"); 
+//      _;
+//     }
+
+//    function withdraw(uint256 rewardToken) external isSevenDay
+//    {
+    
+//     require(rewardToken > 0, "you dont have a rewardToken"); 
+//     require(deposits[msg.sender]>=_amount, "You don't have enough token you is drow"); 
+//     uint256 calc= (deposits[msg.sender]*totalSupply)/reward[msg.sender]*rewardToken;
+//     deposits[msg.sender]-=calc;
+//     stakingToken.transferFrom(address(this), msg.sender,calc );//
+//     totalSupply -= calc; 
+// }
+}
