f6816b33e691a43d2b2378e3bf0c56f88d128a08
Riki3248 <rw740643@gmail.com>
2024-05-12 22:07:51 +0300

    update tender

--- src/tender/tender.sol
+++ src/tender/tender.sol
@@ -1,62 +1,85 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "forge-std/console.sol";
+import "../../new-project/src/MyToken.sol";
 import "../../new-project/src/MyTokenNft.sol";
 
-contract Tender {
-    MyTokenNft nft;
-    mapping(address => uint256) users;
-    mapping(uint256 => address) indexes;
-    uint256 counter;
-    uint256 duration = 7 days;
-    address public maxValue;
-    address payable public owner;
 
-    constructor(address Nft) {
-        address owner = msg.sender;
-        counter = 1;
-        nft = MyTokenNft(Nft);
-        users[owner] = 100;
+contract Tender {
+    address payable public owner;
+    mapping(address => uint256) public users;
+    mapping(uint256 => address) public counter;
+    int256 wad = 10 ** 18;
+    MyTokenNft public immutable myCoinNFT;
+    MyToken public immutable myCoineE;
+    address public maxValue;
+    uint256 public start = 0;
+    uint256 public duration = 7 days;
+    // uint256 public start = block.timestamp;
+    uint256 public count;
+    bool public finish = false;
+
+    constructor(address _myCoinNFT, address _myCoineE, uint256 amount) {
+        myCoinNFT = MyTokenNft(_myCoinNFT);
+        myCoineE = MyToken(_myCoineE);
+        owner = payable(msg.sender);
+        console.log("ooooooooooooooooooooooooooo",owner);
+        users[owner] = amount;
         maxValue = owner;
     }
 
-    receive() external payable {}
-
-    function addNft(uint256 amount) private {
-        require(amount > 0, "amount is zero");
-        require(amount > users[maxValue], "The value is less than the initial value");
-        maxValue = msg.sender;
-        payable(msg.sender).transfer(amount);
-        users[msg.sender] = amount;
-        counter += 1;
-        indexes[counter] = msg.sender;
-    }
-
-    function AddOffer(uint256 amount) external {
-        if (block.timestamp >= duration) {
-            endTender();
-        } else {
-            addNft(amount);
-        }
-    }
-
-    modifier isOwner() {
-        require(msg.sender == owner, " isnt owner");
+    modifier openTender() {
+        require(block.timestamp < duration, "time is finish");
         _;
     }
 
-    function removOffer(address user) external isOwner {
-        require(users[user] == users[maxValue], "You cannot withdraw the money since you have the higher value");
-        users[user] = 0;
+    modifier isOwner() {
+        require(msg.sender == owner, "you dont owner");
+        _;
     }
 
-    function endTender() private isOwner{
-        nft.transferFrom(address(this), maxValue, 1);
-        while (counter > 0) {
-            address offer = indexes[counter];
-            uint256 val = users[offer];
-            payable(msg.sender).transfer(val);
-            counter = counter - 1;
+    function setUsers(address user, uint256 amount) external {
+        users[user] = amount;
+        console.log("users[user]", users[user]);
+        console.log("user", user);
+        console.log("user", msg.sender);
+    }
+
+    function addOffer(uint256 amount) external {
+        if (block.timestamp < duration) {
+            require(amount > 0, "amount is zero");
+            require(amount > users[maxValue], "your offer is less from max offer");
+            maxValue = msg.sender;
+            users[maxValue] = amount;
+            counter[count] = msg.sender;
+            myCoineE.transferFrom(msg.sender, address(this), amount);
+            users[msg.sender] = amount;
+            console.log(users[msg.sender]);
+            count++;
+        } else if (!finish) {
+            endTender();
         }
+    }
+
+    function removeOffer(address user) external isOwner openTender {
+        console.log("owner",users[owner]);
+        console.log("users[user]",users[user]);
+        console.log("users[maxValue]",users[maxValue]);
+        require(users[user] < users[maxValue], "you cannot cancel offer becuse your offer is higher");
+        users[user] = 0;
+        console.log("usususususus",users[user]);
+
+    }
+
+    function endTender() public {
+        myCoinNFT.transferFrom(address(this), address(maxValue), 1);
+        while (count > 0) {
+            address currentAddress = counter[count - 1];
+            uint256 val = myCoinNFT.balanceOf(address(currentAddress));
+            myCoineE.transferFrom(address(this), msg.sender, val);
+            count--;
+        }
+        finish = true;
     }
 }
---
+++ test/tender/tender.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.20;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@hack/store/store.sol";
+import "../../../../new-project/src/MyToken.sol";
+import "../../../../new-project/src/MyTokenNft.sol";
+import "../../src/tender/tender.sol";
+
+contract TestTender is Test {
+    MyTokenNft public myCoinNFT;
+    MyToken public myCoinE;
+    Tender public tenderA;
+    uint256 wad = 10 ** 18;
+    address guy = vm.addr(123);
+    uint256 public count = 100 * wad;
+
+    function setUp() public {
+        myCoinNFT = new MyTokenNft();
+        myCoinE = new MyToken();
+        tenderA = new Tender(address(myCoinNFT), address(myCoinE), count);
+    }
+
+    function testAddOffer() public {
+        uint256 sum = 200 * wad;
+        vm.startPrank(guy);
+        myCoinE.approve(address(tenderA), sum);
+        myCoinE.mint(address(guy), sum);
+        vm.warp(2 days);
+        tenderA.addOffer(sum);
+        assertEq(tenderA.users(guy), 200 * wad);
+        vm.stopPrank();
+    }
+
+    function testDontAddOffer() public {
+        uint256 sum = 50 * wad;
+        vm.startPrank(guy);
+        myCoinE.approve(address(tenderA), sum);
+        myCoinE.mint(address(guy), sum);
+        vm.warp(2 days);
+        vm.expectRevert();
+        tenderA.addOffer(sum);
+        vm.stopPrank();
+    }
+
+    function testRemoveOffer() public {
+        uint256 sum1 = 200 * wad;
+        uint256 sum2 = 300 * wad;
+        address user1 = vm.addr(1234);
+        address user2 = vm.addr(123);
+        address owner = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496;
+        tenderA.setUsers(user1,sum1);
+        tenderA.setUsers(user2,sum2);
+        vm.startPrank(owner);
+        vm.warp(2 days);
+        tenderA.removeOffer(user1);
+        vm.stopPrank();
+    }
+}
